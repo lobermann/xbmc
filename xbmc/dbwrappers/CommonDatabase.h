2778d8a79b7e833fbeb05e1fc82f0e00d78ebe59
Wolfgang Haupt <haupt.wolfgang@gmail.com>
2016-10-06 19:44:29 +0200

    [videodb] move odb generated headers out of CommonDatabase.h
    Adding the generated odb headers in the CommonDatabase.h creates
    unnecessary dependencies.
    It already broke multithreaded compilation as it makes every part
    of the code that includes CommonDatabase.h will depend on the generated
    odb headers.
    We should avoid that.

--- xbmc/dbwrappers/CommonDatabase.h
+++ xbmc/dbwrappers/CommonDatabase.h
@@ -13,28 +13,7 @@
 
 #include <odb/database.hxx>
 
-#include <odb/odb_gen/ODBBookmark.h>
-#include <odb/odb_gen/ODBBookmark_odb.h>
-#include <odb/odb_gen/ODBFile.h>
-#include <odb/odb_gen/ODBFile_odb.h>
-#include <odb/odb_gen/ODBGenre.h>
-#include <odb/odb_gen/ODBGenre_odb.h>
-#include <odb/odb_gen/ODBMovie.h>
-#include <odb/odb_gen/ODBMovie_odb.h>
-#include <odb/odb_gen/ODBPath.h>
-#include <odb/odb_gen/ODBPath_odb.h>
-#include <odb/odb_gen/ODBPerson.h>
-#include <odb/odb_gen/ODBPerson_odb.h>
-#include <odb/odb_gen/ODBPersonLink.h>
-#include <odb/odb_gen/ODBPersonLink_odb.h>
-#include <odb/odb_gen/ODBRating.h>
-#include <odb/odb_gen/ODBRating_odb.h>
-#include <odb/odb_gen/ODBSet.h>
-#include <odb/odb_gen/ODBSet_odb.h>
-#include <odb/odb_gen/ODBStreamDetails.h>
-#include <odb/odb_gen/ODBStreamDetails_odb.h>
-#include <odb/odb_gen/ODBUniqueID.h>
-#include <odb/odb_gen/ODBUniqueID_odb.h>
+
 
 class DatabaseSettings;
 
